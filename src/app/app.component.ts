3b02a55bd285c57f89f3b734cfe8ea3408a57950
Damir Arh <damir.arh@gmail.com>
2022-07-10 10:08:08 +0200

    Form array in a single component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-forms-cross-component';
+  form = this.formBuilder.group({
+    groupName: ['', Validators.required],
+    members: this.formBuilder.array([]),
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  get members() {
+    return this.form.get('members') as FormArray;
+  }
+
+  addMember() {
+    this.members.push(
+      this.formBuilder.group({
+        memberName: ['', Validators.required],
+      })
+    );
+  }
+
+  removeMember(index: number) {
+    this.members.removeAt(index);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+    NoopAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
